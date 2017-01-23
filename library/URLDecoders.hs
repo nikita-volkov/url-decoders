85835dfb5f08e9f017a1f3f85c267da58dae5296
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-01-23 15:18:45 +0300

    Primitive

--- library/URLDecoders.hs
+++ library/URLDecoders.hs
@@ -1,4 +1,14 @@
 module URLDecoders where
 
-import Prelude
+import BasePrelude
+import qualified URLDecoders.Text as A
+import qualified Data.ByteString as B
+import qualified Data.Text as C
 
+
+{-|
+Decoder of URL-encoded bytes (possibly with UTF8) into Text.
+-}
+utf8 :: B.ByteString -> C.Text
+utf8 =
+  A.urlEncodedBytes
