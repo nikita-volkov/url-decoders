40dfe3f1d01e70548443f4f5afd44eee28156992
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-01-26 13:20:58 +0300

    Finalize

--- library/URLDecoders/BinaryParser.hs
+++ library/URLDecoders/BinaryParser.hs
@@ -3,20 +3,18 @@ module URLDecoders.BinaryParser where
 import BasePrelude
 import BinaryParser
 import Data.Text (Text)
+import Data.Hashable (Hashable)
 import qualified Data.HashMap.Strict as A
 import qualified Data.ByteString as C
 import qualified Data.Text as D
 import qualified URLDecoders.UTF8CharDecoder as E
+import qualified Text.Builder.Char as F
+import qualified Text.Builder.StrictText as G
 
-
-data Association =
-  Association !Key !ArrayMarker !Value
-
-newtype ArrayMarker =
-  ArrayMarker Bool
 
 newtype Key =
   Key Text
+  deriving (Eq, Hashable)
 
 newtype Value =
   Value Text
@@ -30,39 +28,76 @@ data QueryChunk a =
 
 query :: BinaryParser Query
 query =
-  undefined
-
-queryAssociations :: BinaryParser [Association]
-queryAssociations =
-  undefined
-
-association :: BinaryParser Association
-association =
-  undefined
-
-textQueryChunk :: BinaryParser (QueryChunk Text)
-textQueryChunk =
-  undefined
+  fmap Query (process A.empty)
+  where
+    process map =
+      accumulateKey mempty
+      where
+        accumulateKey accumulator =
+          optional charQueryChunk >>= \case
+            Just x -> case x of
+              DecodedQueryChunk char -> appendChar char
+              SpecialQueryChunk char -> case char of
+                '+' -> appendChar ' '
+                '%' -> appendChar '%'
+                '[' -> finalizeArrayDeclaration <|> failure ("Broken array declaration at key \"" <> key <> "\"")
+                '=' -> accumulateValue key mempty
+                '&' -> updateMap key [] True
+                ']' -> failure "Unexpected character: \"]\""
+                _ -> appendChar char
+            Nothing -> if D.null key
+              then return map
+              else updateMap key [] False
+          where
+            appendChar char =
+              accumulateKey (accumulator <> F.char char)
+            finalizeArrayDeclaration =
+              do
+                byteWhichIs 93
+                byte >>= \case
+                  61 -> accumulateValue key mempty
+                  63 -> updateMap key [] True
+                  x -> failure ("Unexpected byte: " <> (fromString . show) x)
+            key =
+              G.charBuilder accumulator
+        accumulateValue key accumulator =
+          optional charQueryChunk >>= \case
+            Just x -> case x of
+              DecodedQueryChunk char -> appendChar char
+              SpecialQueryChunk char -> case char of
+                '+' -> appendChar ' '
+                '%' -> appendChar '%'
+                '&' -> updateMap key [value] True
+                _ -> appendChar char
+            Nothing -> updateMap key [value] False
+          where
+            appendChar char =
+              accumulateValue key (accumulator <> F.char char)
+            value =
+              G.charBuilder accumulator
+        updateMap key value continue =
+          if continue
+            then process (A.insertWith (<>) (Key key) (fmap Value value) map)
+            else return map
 
 byteQueryChunk :: BinaryParser (QueryChunk Word8)
 byteQueryChunk =
   do
     firstByte <- byte
     case firstByte of
+      37 -> DecodedQueryChunk <$> percentEncodedByteBody <|> return (SpecialQueryChunk '%')
       43 -> return (SpecialQueryChunk '+')
       63 -> return (SpecialQueryChunk '&')
       61 -> return (SpecialQueryChunk '=')
-      37 -> DecodedQueryChunk <$> percentEncodedByteBody <|> return (SpecialQueryChunk '%')
+      91 -> return (SpecialQueryChunk '[')
+      93 -> return (SpecialQueryChunk ']')
       _ -> return (DecodedQueryChunk firstByte)
 
 charQueryChunk :: BinaryParser (QueryChunk Char)
 charQueryChunk =
   byteQueryChunk >>= \case
     DecodedQueryChunk x -> DecodedQueryChunk <$> interpretedUTF8CharDecoderWithByte E.decodeByte x
-    SpecialQueryChunk x -> case x of
-      '+' -> return (DecodedQueryChunk ' ')
-      '%' -> return (DecodedQueryChunk '%')
-      _ -> return (SpecialQueryChunk x)
+    SpecialQueryChunk x -> return (SpecialQueryChunk x)
 
 interpretedUTF8CharDecoderWithByte :: E.Decoder -> Word8 -> BinaryParser Char
 interpretedUTF8CharDecoderWithByte decoder x =
