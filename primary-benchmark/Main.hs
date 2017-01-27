b3be83985debfe572b301b5267798ae2600da85e
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-01-27 16:55:36 +0300

    Separate "primary-benchmark" and "comparison-benchmark"

---
+++ primary-benchmark/Main.hs
@@ -0,0 +1,24 @@
+module Main where
+
+import Prelude
+import Criterion.Main
+import qualified URLDecoders as A
+import qualified Network.HTTP.Types.URI as E
+
+
+main =
+  defaultMain $
+  [
+    bench "" $ nf A.query $! inputOfSize 10
+  ]
+
+
+-- * Inputs
+-------------------------
+
+inputOfSize :: Int -> ByteString
+inputOfSize size =
+  E.renderQuery False $
+  E.queryTextToQuery $
+  map (\i -> ("abc" <> (fromString . show) i, Just "Ф漢УЙ")) $
+  [0 .. size]
