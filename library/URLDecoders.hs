d678b8f28f2217237ca38bf409d829b6443db0ea
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-01-28 11:33:17 +0300

    Add docs

--- library/URLDecoders.hs
+++ library/URLDecoders.hs
@@ -7,6 +7,15 @@ import qualified Data.HashMap.Strict as D
 import qualified BinaryParser as E
 import qualified URLDecoders.BinaryParser as F
 
+
+{-|
+Decodes the query part of a URL (the one following the question mark) or
+the content of type @application/x-www-form-urlencoded@,
+immediately applying a UTF8-decoding to it.
+
+Produces a hash map of lists of values, interpreting the keys ending with @[]@
+as arrays, as well as the repititive keys.
+-}
 {-# INLINE query #-}
 query :: B.ByteString -> Either C.Text (D.HashMap C.Text [C.Text])
 query =
